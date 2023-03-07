af885476dbf2e32936b1bc7d266f2eb9e942d838
Skye Gibney <skyejgibney@gmail.com>
2023-03-06 20:29:34 -0800

    Change card effect text on smaller screens

--- src/components/deckbuilder/Card.tsx
+++ src/components/deckbuilder/Card.tsx
@@ -27,15 +27,15 @@ const Card = ({ card, isSelected, onClick }: Props) => {
 
   return (
     <div
-      className={`relative flex h-[calc(35px*6)] w-[calc(25px*6)] cursor-pointer flex-col justify-between overflow-hidden rounded-lg border-2 border-[discipline] bg-red-600 bg-cover p-1 text-center font-semibold text-white transition-all hover:bg-red-700 md:h-[calc(35px*8)] md:w-[calc(25px*8)] md:p-2`}
+      className={`relative flex h-[calc(35px*6)] w-[calc(25px*6)] cursor-pointer flex-col justify-between overflow-hidden rounded-lg border-2 border-[discipline] bg-red-600 bg-cover text-center font-semibold text-white transition-all hover:bg-red-700 md:h-[calc(35px*8)] md:w-[calc(25px*8)] md:p-2`}
       style={{
         backgroundImage: `url(/images/cards/${card.image})`,
         border: "2px solid " + (isSelected ? "red" : borderColor),
       }}
       onClick={() => onClick()}
     >
-      <p className="text-xs text-white md:text-xl">{card.name}</p>
-      <p className="rounded-lg bg-gray-800 bg-opacity-70 p-1 text-left text-xs text-white md:p-2 md:text-sm">
+      <p className="p-2 text-xs text-white md:text-xl">{card.name}</p>
+      <p className="bg-gray-800 bg-opacity-70 p-1 text-left text-xs text-white md:rounded-lg md:p-2 md:text-sm">
         {card.originalEffects}
       </p>
     </div>
