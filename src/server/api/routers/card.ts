8ae0373bfcc47555b14348f69e0ee6ad00ba8b44
Skye Gibney <skyejgibney@gmail.com>
2023-03-09 17:58:31 -0800

    Implement deck create and update logic on server

--- src/server/api/routers/card.ts
+++ src/server/api/routers/card.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 
 export const cardRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
@@ -12,20 +13,62 @@ export const cardRouter = createTRPCRouter({
       z.object({
         name: z.string(),
         description: z.string(),
-        cards: z.object({
-          name: z.string(),
-          type: z.string(),
-          affinity: z.string(),
-          image: z.string(),
-          originalEffects: z.string(),
-        }),
+        cards: z
+          .object({
+            id: z.string(),
+          })
+          .array(),
       })
     )
     .mutation(({ input, ctx }) => {
-      return ctx.prisma.card.create({
+      return ctx.prisma.deck.create({
         data: {
-          ...input,
+          name: input.name,
+          description: input.description,
+          cards: {
+            connect: input.cards,
+          },
           creatorId: ctx.session.user.id,
+        },
+      });
+    }),
+  replace: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+        description: z.string(),
+        cards: z
+          .object({
+            id: z.string(),
+          })
+          .array(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const deck = await ctx.prisma.deck.findUnique({
+        where: { id: input.id },
+      });
+
+      if (!deck) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      if (deck.creatorId !== input.id) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+        });
+      }
+
+      return ctx.prisma.deck.update({
+        where: { id: input.id },
+        data: {
+          name: input.name,
+          description: input.description,
+          cards: {
+            set: [],
+            connect: input.cards,
+          },
         },
       });
     }),
