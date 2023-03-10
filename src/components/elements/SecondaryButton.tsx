76276f92b0e869d7f5f604c6899f8c07c8d908d3
Skye Gibney <skyejgibney@gmail.com>
2023-03-09 20:14:15 -0800

    SecondaryButton has type='button'

--- src/components/elements/SecondaryButton.tsx
+++ src/components/elements/SecondaryButton.tsx
@@ -8,6 +8,7 @@ type Props = {
 const SecondaryButton = ({ onClick, children }: Props) => {
   return (
     <button
+      type="button"
       className="mx-auto rounded-lg px-3 py-1 transition hover:bg-gray-300 active:bg-gray-400 dark:bg-black dark:hover:bg-gray-800 dark:active:bg-gray-700"
       onClick={onClick ? () => onClick() : undefined}
     >
