a87e7833e5451e62805476c43b19950ba3a07311
Skye Gibney <skyejgibney@gmail.com>
2023-09-15 15:30:19 -0700

    Modify collection page

--- src/pages/collection.tsx
+++ src/pages/collection.tsx
@@ -5,6 +5,7 @@ import { signIn, useSession } from "next-auth/react";
 import { api } from "../utils/api";
 import PrimaryButton from "../components/elements/PrimaryButton";
 import DeckList from "../components/decklist/DeckList";
+import Link from "next/link";
 
 const Collection: NextPage = () => {
   const { data: session } = useSession();
@@ -13,47 +14,82 @@ const Collection: NextPage = () => {
   if (!session)
     return (
       <>
-        <p className="text-center">
+        <Head>
+          <title>DEKT</title>
+          <meta property="og:title" content="DEKT" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://buildwithdekt.com/" />
+          <meta
+            property="og:image"
+            content="https://buildwithdekt.com/images/logo.png"
+          />
+          <meta
+            property="og:description"
+            content="View your collection of Back 4 Blood decks"
+          />
+          <meta name="theme-color" content="#dc2626" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+
+          <meta
+            name="description"
+            content="View your collection of Back 4 Blood decks"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+
+        <main className="py-12 text-center">
           <PrimaryButton onClick={() => void signIn("discord")}>
             Sign In
           </PrimaryButton>{" "}
           to see your collection!
-        </p>
+        </main>
       </>
     );
 
   return (
     <>
       <Head>
-        <title>Collection</title>
-        <meta property="og:title" content={`Collection`} />
+        <title>DEKT</title>
+        <meta property="og:title" content="DEKT" />
         <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://gleeful-pudding-f9510a.netlify.app/"
-        />
+        <meta property="og:url" content="https://buildwithdekt.com/" />
         <meta
           property="og:image"
-          content="https://gleeful-pudding-f9510a.netlify.app/images/cards/Flawless.webp"
+          content="https://buildwithdekt.com/images/logo.png"
         />
-        <meta property="og:description" content="Your deck collection" />
+        <meta
+          property="og:description"
+          content="View your collection of Back 4 Blood decks"
+        />
         <meta name="theme-color" content="#dc2626" />
 
         <meta name="twitter:card" content="summary_large_image" />
 
-        <meta name="description" content="Your deck collection" />
+        <meta
+          name="description"
+          content="View your collection of Back 4 Blood decks"
+        />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        <h1 className="py-12 text-center text-3xl">Your Collection</h1>
-
-        {decks ? (
-          <div className="pb-12">
+        {decks && decks.length > 0 ? (
+          <div>
+            <h1 className="py-12 text-center text-4xl">Your Collection</h1>
             <DeckList decks={decks} />
           </div>
         ) : (
-          <p>Nothing to show!</p>
+          <p className="py-12 text-center">
+            Nothing in your collection yet! Try{" "}
+            <Link
+              className="rounded-md border-2 border-primary px-1"
+              href="/builder"
+            >
+              Building a Deck
+            </Link>
+          </p>
         )}
       </main>
     </>
